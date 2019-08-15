f9d507b8f3b95b7c74b348b0e81c3fc3fad12ba0
sabyearm <peeranat.long@gmail.com>
2019-08-15 19:48:25 +0700

    Add files via upload

--- bot-checktoken.php
+++ bot-checktoken.php
@@ -5,7 +5,6 @@ $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'MZo3EBaFykn2d1pjlXB2PngXM8InX+ws+tJjPNPPZZIJqkPoGBWbzLO7WViCPfCpkKL6sLZA1P5SAt76PKRfI5Nd/1w5QpEucb1L1l/GAl4HfvAMEUR5go+M7zhWt6ePPauC09nWU28qeX4Xw9V9vAdB04t89/1O/w1cDnyilFU=';// ใส่ Channel Secret
 $channelSecret = 'b08ac2004ac851c096c0214d6b20111e';
 
-
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
@@ -20,17 +19,17 @@ if ( sizeof($request_array['events']) > 0 ) {
         $reply_message = '';
         $reply_token = $event['replyToken'];
 
-        $text = $event['message']['text'];
+
         $data = [
             'replyToken' => $reply_token,
-            // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-            'messages' => [['type' => 'text', 'text' => $text ]]
+            'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
 
         echo "Result: ".$send_result."\r\n";
+        
     }
 }
 
