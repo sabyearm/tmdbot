75dac38ab99074d120b1ca0ae3ad8c5277866534
sabyearm <peeranat.long@gmail.com>
2019-08-15 21:46:59 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,26 +1,158 @@
 <?php
+
+
+function create_content($dtime = "Time" , $temp = "Temp" , $rain = "Rain"){
+
+    $dtime_contents = (object)null;
+    $dtime_contents->type = "box";
+    $dtime_contents->layout = "horizontal";
+    $dtime_contents->contents = array();
+        $sub_dtime = (object)null;
+        $sub_dtime->type = "text";
+        $sub_dtime->text = $dtime;
+        $sub_dtime->size = "sm";
+        $sub_dtime->color = "#555555";
+        $sub_dtime->flex = 0;
+        array_push($dtime_contents->contents , $sub_dtime);
+
+    $Temp_contents = (object)null;
+    $Temp_contents->type = "box";
+    $Temp_contents->layout = "horizontal";
+    $Temp_contents->contents = array();
+        $Temp_contents_main = (object)null;
+        $Temp_contents_main->type = "text";
+        $Temp_contents_main->text = "อุณหภูมิ";
+        $Temp_contents_main->size = "sm";
+        $Temp_contents_main->color = "#555555";
+        $Temp_contents_main->flex = 0
+
+        $Temp_contents_detail = (object)null;
+        $Temp_contents_detail->type = "text";
+        $Temp_contents_detail->text = $temp." °C";
+        $Temp_contents_detail->size = "sm";
+        $Temp_contents_detail->color = "#111111";
+        $Temp_contents_detail->align = "end"
+        array_push($Temp_contents->contents , $Temp_contents_main);
+        array_push($Temp_contents->contents , $Temp_contents_detail);
+
+
+    $Rain_contents = (object)null;
+    $Rain_contents->type = "box";
+    $Rain_contents->layout = "horizontal";
+    $Rain_contents->contents = array();
+        $Rain_contents_main = (object)null;
+        $Rain_contents_main->type = "text";
+        $Rain_contents_main->text = "ปริมาณฝน";
+        $Rain_contents_main->size = "sm";
+        $Rain_contents_main->color = "#555555";
+        $Rain_contents_main->flex = 0;
+
+        $Rain_contents_detail = (object)null;
+        $Rain_contents_detail->type = "text";
+        $Rain_contents_detail->text = $rain." mm.";
+        $Rain_contents_detail->size = "sm";
+        $Rain_contents_detail->color = "#111111";
+        $Rain_contents_detail->align = "end";
+        array_push($Rain_contents->contents , $Rain_contents_main);
+        array_push($Rain_contents->contents , $Rain_contents_detail);
+
+    $Seperator = (object)null;
+    $Seperator->type = "seperator";
+    $Seperator->margin = "xxl";
+
+    $return_array = array();
+    array_push($return_array , $dtime_contents);
+    array_push($return_array , $Temp_contents);
+    array_push($return_array , $Rain_contents);
+    array_push($return_array , $Seperator);
+
+    return $return_array;
+
+}
+
+function main_flex($Location_name = '' , $Location_address = '' , $data){
+
+    $flex = (object)null;
+    $flex->type = "bubble";
+    $flex->style = (object)null;
+        $flex->style->footer = (object)null;
+            $flex->style->footer->separator = true;
+    $flex->body = (object)null;
+        $flex->body->type = "box";
+        $flex->body->layout = "vertical";
+        $flex->body->contents = array();
+            // Forecast_Header
+            $Forecast_Header = (object)null;
+            $Forecast_Header->type = "text";
+            $Forecast_Header->text = "ผลพยากรณ์อากาศ";
+            $Forecast_Header->weight = "bold";
+            $Forecast_Header->color = "#1DB446";
+            $Forecast_Header->size = 'sm';
+            array_push($flex->body->contents , $Forecast_Header);
+            // Forecast_Location
+            $Forecast_Location = (object)null;
+            $Forecast_Location->type = "text";
+            $Forecast_Location->text = $Location_name;
+            $Forecast_Location->weight = "bold";
+            $Forecast_Location->size = "xxl";
+            $Forecast_Location->margin = "md";
+            array_push($flex->body->contents , $Forecast_Location);
+
+            // Forecast_Address
+            $Forecast_Address = (object)null;
+            $Forecast_Address->type = "text";
+            $Forecast_Address->text = $Location_address;
+            $Forecast_Address->color = "#aaaaaa";
+            $Forecast_Address->size = "xs";
+            $Forecast_Address->wrap = true;
+            array_push($flex->body->contents , $Forecast_Address);
+
+            # Seperator
+            $seperator = (object)null;
+            $seperator->type = "seperator";
+            $seperator->margin = "xxl";
+            array_push($flex->body->contents , $seperator);
+
+            $body_content = (object)null;
+            $body_content->type = "box";
+            $body_content->layout = "vertical";
+            $body_content->margin = "xxl";
+            $body_content->spacing = "sm";
+            $body_content->contents = array()
+            for($i = 0 ; $i < sizeof($data) ; $i++){
+                $ts = $forecast_data[$i]->time;
+                $data = $forecast_data[$i]->data;
+                $Temp = $data->tc;
+                $Rain = $data->rain;
+                array_push($body_content->contents ,create_content($ts , $Temp , $Rain));
+            }
+            array_push($flex->body->contents , $body_content);
+
+           return $flex;
+}
+
 function get_api_tmd($lat , $lon){
-$curl = curl_init();
+    $curl = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://data.tmd.go.th/nwpapi/v1/forecast/location/hourly/at?lat=".$lat."&lon=".$lon."&fields=tc,rain&hour=8&duration=2",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => array(
-    "accept: application/json",
-    "authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijc4NTFhYzBmZDhmMTk2YjU0YmMxYWM3Y2ZlODBhODk5OTU5MWZjNzA2OWUzYjBmMjRhZjZhYWI2NDA2NzNiYTk3NmFjZWU3ODBlODVjMjI0In0.eyJhdWQiOiIyIiwianRpIjoiNzg1MWFjMGZkOGYxOTZiNTRiYzFhYzdjZmU4MGE4OTk5NTkxZmM3MDY5ZTNiMGYyNGFmNmFhYjY0MDY3M2JhOTc2YWNlZTc4MGU4NWMyMjQiLCJpYXQiOjE1NjU4NzQzOTgsIm5iZiI6MTU2NTg3NDM5OCwiZXhwIjoxNTk3NDk2Nzk4LCJzdWIiOiIxNzYiLCJzY29wZXMiOltdfQ.j8K9jVu8SrvPTBYaaPYcG3Kf5UxStp7kx9IpLBgLZftIgCwwy8mU70x4pksk5SL17LJN04xGwmLBmXtKtFAfQ8O4V0n4nK-SDG2i5aaXiLPHxy9hR8KI5GPIwp9ayzI6Y-kLU4aa7BotN2UaCFnJwQmyw6lVja24UdvniNfUgB4xpFQP9mDSgVzG_0veefkqeocGnZiUtLvlLtS4eGjB4qzp5w8jMqh2WXRmg9Xia7b33Dh5OQCEWnD-uQYGX8-Ix7v30-B3smQjb-ulrd30tcAcmKxLofTfBwehZirakXMDt-oN0_HqigOBNla69CFm5OJfWdmkkAorWVtSjl4f72xlOEBS-DgXYodtgGQOrDrgFAiEnTqK0AipQwT-KahIpIT7Gv1G9pvFpDp5L-zlCoZaiy0P0IcQKFvsP09BPD020JJUKxHIrO8znOgD3QSR0Q39Y2okBx1WD2nY0yq6zgRRS6IDhmSI-Zbw2Tp9J0x_HjAxmo6RtMqW-Yk7ZosmE0KeY9GHMIcw_fPLSwBPWz2FNkUTgxlwrSP0Yf5Ngzo9gtnwwiOLTfSGLT4UdRq2Q92E_xplwEeAmuXlSkil8zo0fKe1mln2lLxFvCsVd7E2SY3VpaON0-5mYE68AyiEQ_1_fb0rH2E3lRx1gg3qdvYamJD1GL0oTD6aqVMEMDY",
-  ),
-));
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://data.tmd.go.th/nwpapi/v1/forecast/location/hourly/at?lat=".$lat."&lon=".$lon."&fields=tc,rain&hour=8&duration=2",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+        "accept: application/json",
+        "authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijc4NTFhYzBmZDhmMTk2YjU0YmMxYWM3Y2ZlODBhODk5OTU5MWZjNzA2OWUzYjBmMjRhZjZhYWI2NDA2NzNiYTk3NmFjZWU3ODBlODVjMjI0In0.eyJhdWQiOiIyIiwianRpIjoiNzg1MWFjMGZkOGYxOTZiNTRiYzFhYzdjZmU4MGE4OTk5NTkxZmM3MDY5ZTNiMGYyNGFmNmFhYjY0MDY3M2JhOTc2YWNlZTc4MGU4NWMyMjQiLCJpYXQiOjE1NjU4NzQzOTgsIm5iZiI6MTU2NTg3NDM5OCwiZXhwIjoxNTk3NDk2Nzk4LCJzdWIiOiIxNzYiLCJzY29wZXMiOltdfQ.j8K9jVu8SrvPTBYaaPYcG3Kf5UxStp7kx9IpLBgLZftIgCwwy8mU70x4pksk5SL17LJN04xGwmLBmXtKtFAfQ8O4V0n4nK-SDG2i5aaXiLPHxy9hR8KI5GPIwp9ayzI6Y-kLU4aa7BotN2UaCFnJwQmyw6lVja24UdvniNfUgB4xpFQP9mDSgVzG_0veefkqeocGnZiUtLvlLtS4eGjB4qzp5w8jMqh2WXRmg9Xia7b33Dh5OQCEWnD-uQYGX8-Ix7v30-B3smQjb-ulrd30tcAcmKxLofTfBwehZirakXMDt-oN0_HqigOBNla69CFm5OJfWdmkkAorWVtSjl4f72xlOEBS-DgXYodtgGQOrDrgFAiEnTqK0AipQwT-KahIpIT7Gv1G9pvFpDp5L-zlCoZaiy0P0IcQKFvsP09BPD020JJUKxHIrO8znOgD3QSR0Q39Y2okBx1WD2nY0yq6zgRRS6IDhmSI-Zbw2Tp9J0x_HjAxmo6RtMqW-Yk7ZosmE0KeY9GHMIcw_fPLSwBPWz2FNkUTgxlwrSP0Yf5Ngzo9gtnwwiOLTfSGLT4UdRq2Q92E_xplwEeAmuXlSkil8zo0fKe1mln2lLxFvCsVd7E2SY3VpaON0-5mYE68AyiEQ_1_fb0rH2E3lRx1gg3qdvYamJD1GL0oTD6aqVMEMDY",
+    ),
+    ));
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
 
-curl_close($curl);
-return json_decode($response);
+    curl_close($curl);
+    return json_decode($response);
 }
 
 $API_URL = 'https://api.line.me/v2/bot/message';
@@ -55,12 +187,17 @@ if ( sizeof($request_array['events']) > 0 ) {
 			$Temp = $data->tc;
 			$Rain = $data->rain;
 			$text .=  'เวลา'.$ts.'\nอุณหภูมิ = '.$ts.'องศาเซลเซียส\nปริมาณฝน = '.$Rain.'mm\n----------\n';
-		}
+        }
+        $get_flex_api = main_flex('Location' , 'Address' , $forecast_data)
 		$data = [
 		    'replyToken' => $reply_token,
 		    // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
 		    'messages' => [['type' => 'text', 'text' => $text ]]
-		];
+        ];
+        
+        $post_body = json_encode($get_flex_api, JSON_UNESCAPED_UNICODE);
+
+        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
 		
 	}
 	else{
@@ -73,6 +210,10 @@ if ( sizeof($request_array['events']) > 0 ) {
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+        
+
+        echo "Result: ".$send_result."\r\n";
+        
 
         echo "Result: ".$send_result."\r\n";
     }
